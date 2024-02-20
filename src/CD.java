943b19f46701d371dd07bc376bb2e74f56e9ed75
Maxwell Barrett <max_barrett2@yahoo.com>
2024-02-20 12:47:56 -0600

    Additional Classes
    Added the CD class also started the Statement class and made some changes to the Checking/Savings sub-classes

---
+++ src/CD.java
@@ -0,0 +1,10 @@
+public class CD extends Account {
+    double interestRate;
+    String withdrawDate;
+
+    //gets the withdraw date
+    String checkWithdrawDate()
+    {
+        return withdrawDate;
+    }
+}
---
+++ src/Statement.java
@@ -0,0 +1,10 @@
+public class Statement {
+
+    //this should get all attributes associated with Account
+    Account accountUsed;
+    
+    void printMonthlyStatement()
+    {
+        //will print out a detailed monthly statement
+    }
+}
